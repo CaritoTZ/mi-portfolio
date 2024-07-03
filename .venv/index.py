e1188927d3c993b385600c7c6a77c6401c993d6d
CaritoTZ <CaritoTZ@users.noreply.github.com>
2024-07-03 15:50:45 -0300

    commit inicial

---
+++ .venv/index.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+"""
+app=Flask(__name__)
+@app.route('/')
+def principal():
+    return "Mi portfolio"
+@app.route('/contacto')
+def contacto():
+    return "Esta es la pagina de contacto"
+"""
+app=Flask(__name__)
+@app.route('/')
+def principal():
+    return render_template('index.html')
+
+@app.route('/lenguajes')
+def mostrarlenguajes():
+    mislenguajes=("Pyhton", "Java", "C#", "JavaScripts")
+    return render_template('lenguajes.html', lenguajes=mislenguajes)
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+if __name__ =='_main_':
+    app.run(debug=True)
