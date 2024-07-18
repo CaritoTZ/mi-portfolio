856250f41fd467d9ea306cad82466abb6ba27c36
CaritoTZ <CaritoTZ@users.noreply.github.com>
2024-07-17 21:48:55 -0300

    se agrega experiencia

--- .venv/index.py
+++ .venv/index.py
@@ -11,6 +11,10 @@ def mostrarlenguajes():
     mislenguajes=("Pyhton", "Java", "C#", "JavaScripts")
     return render_template('lenguajes.html', lenguajes=mislenguajes)
 
+@app.route('/experiencia.html')
+def experiencia():
+    return render_template('experiencia.html')
+
 @app.route('/proyectos')
 def proyectos():
     return render_template('proyectos.html')
