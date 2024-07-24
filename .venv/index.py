988b9345123f191081ddf1cc576b09d0e9f5c0d4
CaritoTZ <CaritoTZ@users.noreply.github.com>
2024-07-24 18:37:13 -0300

    Se agregan los iconos

--- .venv/index.py
+++ .venv/index.py
@@ -4,16 +4,18 @@ from flask import Flask, render_template
 app=Flask(__name__)
 @app.route('/')
 def principal():
-    return render_template('index.html')
+    title = "Mi Portfolio"
+    return render_template('index.html', title=title)
 
-@app.route('/lenguajes')
-def mostrarlenguajes():
+
+@app.route('/skills')
+def skills():
     mislenguajes=("Pyhton", "Java", "C#", "JavaScripts")
     return render_template('lenguajes.html', lenguajes=mislenguajes)
 
 @app.route('/experiencia')
 def experiencia():
-    return render_template('experiencia.html', seccion=experiencia)
+    return render_template('experiencia.html')
 
 @app.route('/proyectos')
 def proyectos():
