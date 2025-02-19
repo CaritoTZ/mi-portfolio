0025109653dbd036723c8a253544dc2f578334d4
CaritoTZ <CaritoTZ@users.noreply.github.com>
2025-02-19 18:58:45 -0300

    index corregido

--- .venv/index.py
+++ .venv/index.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 
 app=Flask(__name__)
-@app.route('/mi-portfolio')
+@app.route('/')
 def principal():
     title = "Mi Portfolio"
     return render_template('index.html', title=title)
